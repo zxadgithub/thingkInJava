e0ce725d54f5709be67d0ae8599a33415f888d62
张新安 <xin_anzhang@126.com>
2017-06-10 00:05:58 +0800

    test

--- thinking/src/zxa/io/dir/DirList.java
+++ thinking/src/zxa/io/dir/DirList.java
@@ -5,7 +5,7 @@ import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-
+//测试
 public class DirList {
 	public static void main(String[] args){
 		System.out.println("输入文件目录");
