3a41cfbb9860f1d17983ca6464d31f3dd6d31b61
张新安 <xin_anzhang@126.com>
2017-06-10 11:36:40 +0800

    匿名内部类实现

--- thinking/src/zxa/io/dir/DirList2.java
+++ thinking/src/zxa/io/dir/DirList2.java
@@ -1,28 +1,41 @@
 package zxa.io.dir;
 
-
-//page526
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-//测试
-public class DirList {
-	public static void main(String[] args){
+
+public class DirList2 {
+	
+	
+	public static FilenameFilter filter(final String regex){
+		//匿名内部类实现
+		return new FilenameFilter(){
+			private Pattern pattern = Pattern.compile(regex);
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				return pattern.matcher(name).matches();
+			}
+			
+		};
+	}
+	
+	public static void main(String[] args) {
 		System.out.println("输入文件目录");
 		Scanner scanner = new Scanner(System.in);
 		
 		String file = scanner.next();
 		File path = new File(file);
 		String[] list;
-		System.out.println("输入正则表达式搜索");
+		System.out.println("输入正则表达式搜索（/代表所有文件）");
 		String pa = scanner.next();
-		if(pa.length() == 0){
+		if(pa.equals("/")){
 			list = path.list();
 		}else{
 			//实现文件匹配规则
-			list = path.list(new DirFilter(pa));
+			list = path.list(filter(pa));
 		}
 		//list排序以及排序规则
 		Arrays.sort(list,String.CASE_INSENSITIVE_ORDER);
@@ -32,20 +45,6 @@ public class DirList {
 		if(scanner!=null)
 		scanner.close();
 	}
-}
-
-class DirFilter implements FilenameFilter{
-	private Pattern pattern;
 	
-	public DirFilter(String regex) {
-		pattern = Pattern.compile(regex);
-	}
-	@Override
-	public boolean accept(File arg0, String arg1) {
-		return pattern.matcher(arg1).matches();
-	}
 	
 }
-
-
-
